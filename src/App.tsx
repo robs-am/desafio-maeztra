332966530239659d2d37596242faeb38d9975bb5
Roberta <roberta.amarto@ed3.com.br>
2024-05-17 10:32:23 -0300

    feat: adiciona estrutura inicial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl'>Hello</h1>
+    </>
+  )
+}
+
+export default App
