bee15f178eaa6e58b3b6682beedd13a645d873a7
Roberta <roberta.amarto@ed3.com.br>
2024-05-18 14:33:16 -0300

    feat: adiciona data navigation

--- src/data.jsx
+++ src/data.jsx
@@ -4,3 +4,13 @@ export const hero = {
   title: 'Welcome to the future',
   subtitle: 'The best place to find the best products',
 };
+
+export const navigation = [
+  { name: 'Novidades', href: '#' },
+  { name: 'Vestidos', href: '#' },
+  { name: 'Roupas', href: '#' },
+  { name: 'Sapatos', href: '#' },
+  { name: 'Lingerie', href: '#' },
+  { name: 'Acessórios', href: '#' },
+  { name: 'Outlet', href: '#' },
+];
