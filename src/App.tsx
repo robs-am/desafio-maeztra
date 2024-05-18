ff23528828e3614100628d40862f4036600c002f
Roberta <roberta.amarto@ed3.com.br>
2024-05-18 08:24:34 -0300

    style: adiciona fonte e favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import Header from './components/Header/Header'
 
 
 import './App.css'
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <Header />
       <h1 className='text-3xl'>Hello</h1>
     </>
   )
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+  return (
+    <ul>
+      <li>Teste</li>
+      <li>Teste</li>
+      <li>Teste</li>
+      <li>Teste</li>
+      <li>Teste</li>
+    </ul>
+  )
+}
+
+export default Header
