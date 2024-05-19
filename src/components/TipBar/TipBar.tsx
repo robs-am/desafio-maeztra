4fcd80ca166e6d393664c9aefc293bad06d5401d
Roberta <roberta.amarto@ed3.com.br>
2024-05-18 21:21:45 -0300

    style? ajusta responsividade banner

--- src/components/TipBar/TipBar.tsx
+++ src/components/TipBar/TipBar.tsx
@@ -1,31 +1,29 @@
+import React from 'react'
+
 import React, { useEffect } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Banner from '../../assets/banner/banner.png';
-import BannerMobile from '../../assets/banner/banner-mobile.png';
 import Autoplay from 'embla-carousel-autoplay';
-import './global.css';
 
-export function EmblaCarousel() {
+
+export function TipBar() {
   const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
         <div className="embla__slide">
-          <img className="hidden lg:block" src={Banner} />
-          <img className="block w-full  lg:hidden" src={BannerMobile} />
+          <img src={Banner} />
         </div>
         <div className="embla__slide">
-          <img className="hidden lg:block" src={Banner} />
-          <img className="block w-full  lg:hidden" src={BannerMobile} />
+          <img src={Banner} />
         </div>
         <div className="embla__slide">
-          <img className="hidden lg:block" src={Banner} />
-          <img className="block w-full lg:hidden" src={BannerMobile} />
+          <img src={Banner} />
         </div>
       </div>
     </div>
   );
 }
 
-export default EmblaCarousel;
+export default TipBar
